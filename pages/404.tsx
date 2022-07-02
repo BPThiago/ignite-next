108c5d0f12e72e8e7f2226d1f549607e6e5f4dea
Thiago Borges <thiagoborges980@gmail.com>
2022-07-01 21:56:20 -0300

    Smartphone font in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,14 +4,14 @@ export default function Page404() {
     return (
         <div className="bg-gray-700 text-gray-100">
             <div className="mx-auto grid grid-cols-1 md:grid-cols-2 min-h-screen items-center p-4 gap-5 max-w-[1100px]">
-                <div className="flex flex-col items-center text-6xl uppercase font-extrabold gap-5">
+                <div className="flex flex-col items-center text-5xl md:text-6xl uppercase font-extrabold gap-5">
                     <span>Don't panic!</span>
                     <div className="relative md:w-[12rem] md:h-[18rem]">
                         <Image src="/images/marvin.png" layout="fill" className="-scale-x-100"/>
                     </div>
                 </div>
                 <div className="flex flex-col gap-4 text-lg max-w-[500px]">
-                    <span className="uppercase font-bold text-purple-600 text-6xl mb-6">Código 4-0-4</span>
+                    <span className="uppercase font-bold text-purple-600 text-5xl md:text-6xl mb-6">Código 4-0-4</span>
                     <span className="text-3xl font-semibold uppercase">Não é um teste, repito, não é um teste!</span>
                     <span className="text-green-400">Administrador responde:</span>
                     <p className="">Por mais que você esteja buscando a resposta da vida, do universo e de tudo mais, você foi longe demais...</p>
